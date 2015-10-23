4acee0dd5ac7b1b92edf7526fc18aea6624121ca
Luis Felipe Souza <luisfmsouza@gmail.com>
2015-10-23 09:55:07 -0200

    Issue #41: Inclding editorconfig

--- gulp-tasks/js-eslint.js
+++ gulp-tasks/js-eslint.js
@@ -3,24 +3,24 @@
 module.exports = function (gulp, $, config) {
 
   return function() {
-        gulp.src(config.paths.scripts.src + '*.js')
+    gulp.src(config.paths.scripts.src + '*.js')
 
-        // Eslint
-        .pipe($.eslint())
-        .pipe($.eslint.format())
-        .pipe($.eslint.failAfterError())
-    
-        .pipe($.sourcemaps.init())
-        
-        // Concat and rename
-        .pipe($.concat(config.paths.scripts.fileName))
-        .pipe($.rename({
-            suffix: '.min'
-        }))
+    // Eslint
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+    .pipe($.eslint.failAfterError())
 
-        // uglifying and moving to destination
-        .pipe($.uglify())
-        .pipe($.sourcemaps.write('.'))
-        .pipe(gulp.dest(config.paths.scripts.dest));
-    };
+    .pipe($.sourcemaps.init())
+
+    // Concat and rename
+    .pipe($.concat(config.paths.scripts.fileName))
+    .pipe($.rename({
+        suffix: '.min'
+    }))
+
+    // uglifying and moving to destination
+    .pipe($.uglify())
+    .pipe($.sourcemaps.write('.'))
+    .pipe(gulp.dest(config.paths.scripts.dest));
+  };
 };
