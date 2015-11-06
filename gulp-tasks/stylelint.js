57ffb724adfc44e6e5cb30831af6fe221310aa4e
Luis Felipe Souza <luisfmsouza@gmail.com>
2015-11-06 15:08:41 -0200

    Closes #38: Arquivo de configuração externo pro stylelint funcinando o/

--- gulp-tasks/stylelint.js
+++ gulp-tasks/stylelint.js
@@ -13,26 +13,7 @@ module.exports = function (gulp, $, config) {
   return function() {
     gulp.src(config.paths.styles.dest + "*.css")
     .pipe($.postcss([
-      stylelint({
-        "rules": {
-          "color-no-invalid-hex": 2,
-          "color-hex-case": [2, "lower"],
-          "color-hex-length": [2, "short"],
-          "block-no-empty": 2,
-          "declaration-colon-space-before": [2, "never"],
-          "declaration-no-important": 2,
-          "indentation": [2, 2],
-          "function-space-after": [2, "always"],
-          "function-url-quotes": [2, "double"],
-          "no-missing-eof-newline": 2,
-          "rule-no-duplicate-properties": 2,
-          "rule-properties-order": [2, "alphabetical"],
-          "selector-no-id": 2,
-          "selector-no-universal": 1,
-          "selector-pseudo-element-colon-notation": [2, "single"],
-          "string-quotes": [2, "double"]
-        }
-      }),
+      stylelint(),
       reporter({
         clearMessages: true,
       })
