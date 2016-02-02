c7282fa2984cfd7f3e548c70c8fc818aad9520c0
W@lterCS <waltercassiano@gmail.com>
2016-02-02 10:17:03 -0200

    Npm Test

--- mymodules/check_version.js
+++ mymodules/check_version.js
@@ -1,65 +1,75 @@
 'use strict';
 var childProcess = require('child_process');
 var util = require('util');
+var cp = require('child_process');
 
-var options = {
-  	npm: '3.5.2',
-	node: '4.2.2'
-}
+
+var engines = {
+    node : process.env.npm_package_engines_node || undefined,
+    npm : process.env.npm_package_engines_npm || undefined
+};
+
+var localEnv = {
+  node : process.env.npm_config_user_agent.match(/node[//a-z_0-9_.]+/g) || undefined,
+  npm : process.env.npm_config_user_agent.match(/npm[//a-z_0-9_.]+/g) || undefined,
+};
+
 
 var checkversion = function() {
-	this.defaults = {
-		npm: '3.3.3',
-		node: '3.3.3'		
-	},
-	this.init = function(options) {
-		var scope = this;
-		scope.defaults = options || {};
-		
-		childProcess.exec('npm -v', function(error, stdout,stderr) {
-  		  if(error)
-    		return error
-    	  scope.callBack('npm', stdout);
-		});
+	this.engines,
+  this.localEnv,
+	this.init = function(engines,localEnv) {
 
-		childProcess.exec('node -v', function(error, stdout, stderr){
-		  if(error)
-		    return error;
-		  scope.callBack('node', stdout);
-		});
+		var scope = this;
+		scope.engines = engines || undefined;
+    scope.localEnv = localEnv || undefined;
+
+    //Exit if not get version from system or package json
+    if( !scope.engines || !scope.localEnv )
+      process.exit(8);
+
+    //Remove v4.*.*
+    scope.callBack('node', scope.localEnv.node.toString() );
+    scope.callBack('npm', scope.localEnv.npm.toString() );
+    //test success
+    process.exit(0);
+
 	},
 	this.callBack = function(elementVersion, version) {
 		var scope = this;
 		var npm;
 		var node;
+
 		switch (elementVersion) {
 			case 'node': {
 				node = version.replace(/([^0-9.]|\r\n|\n|\r)/gm,"");
-				console.log('Current Version node: ' + node + ' => Version recomended ' + ((scope.defaults[elementVersion]) ? scope.defaults[elementVersion] : '' ));
-				this.stopInstall(elementVersion,node);
+				console.log('- Current Version node: ' + node + ' => Version recomended ' + ((scope.engines[elementVersion]) ? scope.engines[elementVersion] : '' ));
+				this.testVersion(elementVersion, node);
 				break;
 			}
 			case 'npm' : {
 				npm = version.replace(/([^0-9.]|\r\n|\n|\r)/gm,"");
-				console.log('Current Version node: ' + npm + ' => Version recomended ' + ((scope.defaults[elementVersion]) ?  scope.defaults[elementVersion] : '' ));
-				this.stopInstall(elementVersion,npm);
+				console.log('- Current Version npm: ' + npm + ' => Version recomended ' + ((scope.engines[elementVersion]) ?  scope.engines[elementVersion] : '' ));
+				this.testVersion(elementVersion, npm);
 				break;
 			}
-			default: 'Node is not installed'
+			default: process.exit(0);
 		}
 	},
-	this.stopInstall = function(elementVersion,version) {
+	this.testVersion = function(elementVersion,version) {
 		var scope = this;
-		if (version < scope.defaults[elementVersion]) {
-			console.log(elementVersion + scope.defaults[elementVersion]);
-			process.exit(8);
+		if (version < scope.engines[elementVersion].replace(/([^0-9.]|\r\n|\n|\r)/gm,"")) {
+			process.exit(1);
 		}
 	}
 }
 
 process.on('exit', function(code) {
-
+  switch (code) {
+    case 0 : console.log('\nTest Succcess!!! All requirements are ok and you can run npm install now.'); break;
+    case 1 : console.log('\nPlease confirm above requirements'); break;
+    default: return;
+  }
 });
 
-
-module.exports = new checkversion().init(options);
+module.exports = new checkversion().init(engines,localEnv);
