3e0420f1422ca2ef5e0c383c04422627a489c497
Walter Cassiano <Walter Cassiano>
2016-02-01 23:38:44 -0200

    Npm test

--- mymodules/check_version.js
+++ mymodules/check_version.js
@@ -1,21 +1,30 @@
 'use strict';
 var childProcess = require('child_process');
+var util = require('util');
+
+var options = {
+  	npm: '3.5.2',
+	node: '4.2.2'
+}
 
 var checkversion = function() {
-	this.defaults,
+	this.defaults = {
+		npm: '3.3.3',
+		node: '3.3.3'		
+	},
 	this.init = function(options) {
 		var scope = this;
 		scope.defaults = options || {};
-		console.log(childProcess);
-		childProcess.execSync('npm -v', function(error, stdout,stderr) {
+		
+		childProcess.exec('npm -v', function(error, stdout,stderr) {
   		  if(error)
     		return error
     	  scope.callBack('npm', stdout);
 		});
-		childProcess.execSync('node -v', function(error, stdout, stderr){
+
+		childProcess.exec('node -v', function(error, stdout, stderr){
 		  if(error)
 		    return error;
-				console.log('here');
 		  scope.callBack('node', stdout);
 		});
 	},
@@ -26,17 +35,31 @@ var checkversion = function() {
 		switch (elementVersion) {
 			case 'node': {
 				node = version.replace(/([^0-9.]|\r\n|\n|\r)/gm,"");
-				console.log('Current Version node: ' + node + ' =>>> Version required ' + ((scope.defaults.node) ? scope.defaults.node : '' ));
+				console.log('Current Version node: ' + node + ' => Version recomended ' + ((scope.defaults[elementVersion]) ? scope.defaults[elementVersion] : '' ));
+				this.stopInstall(elementVersion,node);
 				break;
 			}
 			case 'npm' : {
 				npm = version.replace(/([^0-9.]|\r\n|\n|\r)/gm,"");
-				console.log('Current Version node: ' + npm + ' =>>> Version required ' + ((scope.defaults.npm) ?  scope.defaults.npm : '' ));
+				console.log('Current Version node: ' + npm + ' => Version recomended ' + ((scope.defaults[elementVersion]) ?  scope.defaults[elementVersion] : '' ));
+				this.stopInstall(elementVersion,npm);
 				break;
 			}
 			default: 'Node is not installed'
 		}
+	},
+	this.stopInstall = function(elementVersion,version) {
+		var scope = this;
+		if (version < scope.defaults[elementVersion]) {
+			console.log(elementVersion + scope.defaults[elementVersion]);
+			process.exit(8);
+		}
 	}
 }
 
-module.exports = new checkversion();
+process.on('exit', function(code) {
+
+});
+
+
+module.exports = new checkversion().init(options);
