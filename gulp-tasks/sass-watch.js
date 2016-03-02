5a98175649bd6e73d35187b91629b2557890d1dc
W@lterCS <waltercassiano@gmail.com>
2016-03-02 09:31:34 -0300

    Sass Watch setup Stylelint

--- gulp-tasks/sass-watch.js
+++ gulp-tasks/sass-watch.js
@@ -2,14 +2,25 @@
 
 module.exports = function (gulp, $, config) {
   return function() {
-    gulp.watch(config.paths.styles.src + '*.scss', ['sass:dev'],'' , function(callBack) {
-      console.log(callBack);
-    });
+    gulp.src(config.paths.styles.src + '/**/*.scss')
+    .pipe(
+      $.watch(config.paths.styles.src + '/**/*.scss').on('change', function(file) {
+        $.util.log('Changed: ' + file);
+        $.runSequence(['stylelint','sass:dev']);
+      }).on('add', function(file) {
+        $.util.log('Added: ' + file);
+        $.runSequence(['stylelint','sass:dev']);
+      }).on('unlink', function(file) {
+        $.util.log('Removed: ' + file);
+        $.runSequence(['stylelint','sass:dev']);
+      })
+    )
+
 
     // Create LiveReload server
     $.livereload.listen();
 
     // Watch any files in dist/, reload on change
-    gulp.watch([config.paths.styles.dest + '**']).on('change', $.livereload.changed);
+    $.watch([config.paths.styles.dest + '**']).on('change', $.livereload.changed);
   };
 };
